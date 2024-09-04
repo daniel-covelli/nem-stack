70d7ec5b522139649d11e88d7e9a751a45e0aa0b
Daniel Covelli <danielcovelli@berkeley.edu>
2024-09-04 16:13:38 -0700

    spin up server with mongodb connection

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,27 @@
+import { config } from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
+import user from './router/user';
+import bodyParser from 'body-parser';
+import { errorHandler } from './middleware/errors';
+
+config()
 
 const app = express();
 const port = 3000;
+const DB_URI = process.env.DB_URI || ""
 
-app.get('/', (req, res) => {
+app.use(bodyParser.json());
+
+mongoose.connect(DB_URI)
+    .then(() => console.log("Connected to MongoDB"))
+
+app.get('/', (_, res) => {
     res.send('Hello, TypeScript with Express!');
 });
+
+app.use('/user', user)
+app.use(errorHandler)
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
